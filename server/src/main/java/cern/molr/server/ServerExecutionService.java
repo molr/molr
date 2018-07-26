0320a51ddbb02e2ce5ce96b45bbf0e1f1b3da056
Yassine Kriouile <ykriouil@yassine-dev.cern.ch>
2018-07-26 10:18:50 +0200

    updates the ServerExecutionService class

--- server/src/main/java/cern/molr/server/ServerExecutionService.java
+++ server/src/main/java/cern/molr/server/ServerExecutionService.java
@@ -75,22 +75,6 @@ public class ServerExecutionService {
         Optional<RemoteMoleSupervisor> optional = supervisorsManager.chooseSupervisor(request.getMissionName());
         return optional.map((supervisor) -> {
             Publisher<MissionEvent> executionEventStream = supervisor.instantiate(request, missionEId);
-            executionEventStream.subscribe(new SimpleSubscriber<MissionEvent>() {
-                @Override
-                public void consume(MissionEvent event) {
-
-                }
-
-                @Override
-                public void onError(Throwable t) {
-
-                }
-
-                @Override
-                public void onComplete() {
-                    registry.removeMissionExecution(missionEId);
-                }
-            });
             registry.registerNewMissionExecution(missionEId, supervisor, executionEventStream);
             return missionEId;
         }).orElseThrow(() ->
@@ -107,7 +91,7 @@ public class ServerExecutionService {
     public Publisher<MissionEvent> getEventsStream(String mEId) throws UnknownMissionException {
         Optional<Publisher<MissionEvent>> optionalStream = registry.getMissionExecutionStream(mEId);
         return Flux.from(optionalStream.orElseThrow(() -> new UnknownMissionException("No such mission running")))
-                .filter(event -> !(event instanceof MissionStateEvent));
+                .filter(event -> !(event instanceof MissionStateEvent)).doOnComplete(() -> registry.removeMissionExecution(mEId));
     }
 
     public Publisher<MissionState> getStatesStream(String mEId) throws UnknownMissionException {
