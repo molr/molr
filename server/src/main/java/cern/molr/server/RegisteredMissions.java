c85a037de3e6f30ce8c7aae4739e40e33d837cdc
Andrej Svec <andrej.svec@cern.ch>
2018-08-02 09:15:10 +0200

    MPESW-18 Add javadoc

--- server/src/main/java/cern/molr/server/RegisteredMissions.java
+++ server/src/main/java/cern/molr/server/RegisteredMissions.java
@@ -3,9 +3,18 @@ package cern.molr.server;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class containing list of missions registered in MolR server registry.
+ *
+ * @author asvec, astanisz
+ */
 public class RegisteredMissions {
     private List<String> missions;
 
+    /**
+     * Instantiates {@link RegisteredMissions}.
+     * @param missions canonical paths of mission classes to be registered in MolR server registry
+     */
     public RegisteredMissions(List<String> missions) {
         this.missions = new ArrayList<>(missions);
     }
