ebf50f4b88a49142951065fbe886ac2471e378c8
kaifox <kajetan.fuchsberger@cern.ch>
2018-08-15 10:14:45 +0200

    refinement field renaming

--- commons-api/src/main/java/org/molr/commons/api/domain/dto/MissionRepresentationDto.java
+++ commons-api/src/main/java/org/molr/commons/api/domain/dto/MissionRepresentationDto.java
@@ -1,8 +1,6 @@
 package org.molr.commons.api.domain.dto;
 
-import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMultimap;
 import org.molr.commons.api.domain.Block;
 import org.molr.commons.api.domain.ImmutableMissionRepresentation;
 import org.molr.commons.api.domain.MissionRepresentation;
@@ -11,7 +9,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.function.Function.identity;
@@ -22,18 +19,18 @@ public class MissionRepresentationDto {
 
     public final String rootBlockId;
     public final Set<BlockDto> blocks;
-    public final Map<String, List<String>> blockChildrenIds;
+    public final Map<String, List<String>> childrenBlockIds;
 
-    public MissionRepresentationDto(String rootBlockId, Set<BlockDto> blocks, Map<String, List<String>> blockChildrenIds) {
+    public MissionRepresentationDto(String rootBlockId, Set<BlockDto> blocks, Map<String, List<String>> childrenBlockIds) {
         this.rootBlockId = rootBlockId;
         this.blocks = blocks;
-        this.blockChildrenIds = blockChildrenIds;
+        this.childrenBlockIds = childrenBlockIds;
     }
 
     public MissionRepresentationDto() {
         this.rootBlockId = null;
         this.blocks = Collections.emptySet();
-        this.blockChildrenIds = Collections.emptyMap();
+        this.childrenBlockIds = Collections.emptyMap();
     }
 
     public static final MissionRepresentationDto from(MissionRepresentation representation) {
@@ -56,7 +53,7 @@ public class MissionRepresentationDto {
 
         ImmutableMissionRepresentation.Builder builder = ImmutableMissionRepresentation.builder(blockMap.get(rootBlockId));
         for (Block block : blockMap.values()) {
-            List<String> childrenIds = blockChildrenIds.get(block.id());
+            List<String> childrenIds = childrenBlockIds.get(block.id());
             if (childrenIds != null) {
                 childrenIds.stream()
                         .map(blockMap::get)
