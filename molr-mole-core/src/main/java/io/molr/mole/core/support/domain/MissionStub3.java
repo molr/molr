57f3e59a6e8d2c2b85483941bce066cb02163e04
kaifox <kajetan.fuchsberger@gmail.com>
2020-08-15 00:45:05 +0200

    Added mission stub for 3 params

---
+++ molr-mole-core/src/main/java/io/molr/mole/core/support/domain/MissionStub3.java
@@ -0,0 +1,62 @@
+package io.molr.mole.core.support.domain;
+
+import io.molr.commons.domain.Mission;
+import io.molr.commons.domain.Placeholder;
+
+import java.util.Map;
+
+/**
+ * Represents a {@link Mission} which accepts thre parameters and returns a value
+ *
+ * @param <P1> the type for parameter 1
+ * @param <P2> the type for parameter 2
+ * @param <P3> the type for parameter 3
+ * @param <R>  the type for return value of {@link Mission} being represented
+ * @author HimanshuSahu31
+ */
+public class MissionStub3<P1, P2, P3, R> {
+
+    private StubData data;
+
+    MissionStub3(Mission mission, Class<R> returnType, Placeholder<P1> p1, Placeholder<P2> p2, Placeholder<P3> p3) {
+        data = StubData.from(mission)
+                .setReturnType(returnType)
+                .addParameter(p1)
+                .addParameter(p2)
+                .addParameter(p3)
+                .build();
+    }
+
+    /**
+     * @return the {@link Mission} being represented
+     */
+    public Mission mission() {
+        return data.mission();
+    }
+
+    /**
+     * @return the type for return value of {@link Mission} being represented
+     */
+    public Class<R> returnType() {
+        return (Class<R>) data.returnType();
+    }
+
+    /**
+     * The parameter map required by the represented {@link Mission} is constructed by the arguments provided.
+     *
+     * @param p1 the parameter 1
+     * @param p2 the parameter 2
+     * @return the parameter map constructed with the arguments provided
+     */
+    public Map<String, Object> parameters(P1 p1, P2 p2, P3 p3) {
+        return data.parameters(p1, p2, p3);
+    }
+
+    /**
+     * @param index the index of parameter
+     * @return the {@link Placeholder} for parameter at given index
+     */
+    protected Placeholder<?> parameterAt(int index) {
+        return data.parameterAt(index);
+    }
+}
---
+++ molr-mole-core/src/main/java/io/molr/mole/core/support/domain/VoidStub3.java
@@ -0,0 +1,16 @@
+package io.molr.mole.core.support.domain;
+
+import io.molr.commons.domain.Mission;
+import io.molr.commons.domain.Placeholder;
+
+public class VoidStub3<P1, P2, P3> extends MissionStub3<P1, P2, P3, Void> {
+
+    VoidStub3(Mission mission, Class<Void> returnType, Placeholder<P1> p1, Placeholder<P2> p2, Placeholder<P3> p3) {
+        super(mission, returnType, p1, p2, p3);
+    }
+
+    public <R> MissionStub3<P1, P2, P3, R> returning(Class<R> returnType) {
+        return new MissionStub3<P1, P2, P3, R>(mission(), returnType,
+                (Placeholder<P1>) parameterAt(0), (Placeholder<P2>) parameterAt(1), (Placeholder<P3>) parameterAt(2));
+    }
+}
